11a0e8a5df6ce59add3c3e10be497c480fbafed0
Mitch Kostich <mitch.kostich@jax.org>
2022-07-21 15:21:29 -0400

    setup for setuptools, cython and ray

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import setuptools
+import Cython.Build
+import sys
+
+"""
+run with: python setup.py build_ext --inplace
+"""
+
+setuptools.setup(
+    ext_modules = Cython.Build.cythonize("resolve.pyx")
+)
+
